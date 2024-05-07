1959a8641ac4339cfa6c33d5b159698646af644d
suweiteng <suweiteng@51talk.com>
2024-05-07 17:06:43 +0800

    支持docx文件

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 
 
-用python进行文本分词并生成词云
-
+用python进行文本分词并生成词云，支持txt和docx格式文件。
 
 ## 安装
 
 * `pip3 install jieba`
 * `pip3 install wordcloud`
+* `pip3 install python-docx`
 
 
 # 运行
 * 运行demo.py,会生成相应的的词云图，和“doc/词频统计.txt”  
-* `python3 demo.py`
-
+* txt文件： `python3 demo.py` 
+* docx文件： `python3 demo.py d`
 
 
 ## 说明
@@ -22,5 +22,6 @@
 
 * 默认无意义词库，如'着'等。可自行添加不想纳入统计的词语。路径：userdict/stopword.txt
 
+* 默认读取demo.txt 如有附加参数d 则读取demo.docx
 
 ![image](https://raw.githubusercontent.com/suweiteng/wordCloud/master/Images/alice.png)  
--- demo.py
+++ demo.py
@@ -1,18 +1,38 @@
 # coding:utf-8
-
+import sys 
 from os import path
+from docx import Document 
 import chnSegment
 import plotWordcloud
 
 
 if __name__=='__main__':
 
-    # 读取文件
+    # 读取文件 默认读取demo.txt 如有附加参数d 则读取demo.docx
     d = path.dirname(__file__)
-    text = open(path.join(d,'doc//alice.txt')).read()
-
+    file_path = 'doc/demo.txt'  #  要读取的文件名称 
+    if len(sys.argv) > 1:  
+        first_arg = sys.argv[1]  
+        print(f"The first argument is: {first_arg}")
+        if first_arg == 'd':
+           file_path = 'doc/demo.docx' 
+    
+  
+    # 获取文件扩展名  
+    file_extension = path.splitext(file_path)   
+    if file_extension[1].lower() == '.txt':  
+        # 读取 .txt 文件  
+        with open(file_path, 'r', encoding='utf-8') as file:  
+            content = file.read()  
+    elif file_extension[1].lower() == '.docx':  
+            # 使用 python-docx 读取 .docx  
+            doc = Document(file_path)  
+            content = '\n'.join([para.text for para in doc.paragraphs])    
+    else:  
+        print( "未知文件类型" ) 
+    
     # 若是中文文本，则先进行分词操作
-    text=chnSegment.word_segment(text)
+    text=chnSegment.word_segment(content)
     
     # 生成词云
     plotWordcloud.generate_wordcloud(text)
