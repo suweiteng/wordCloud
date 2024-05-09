b1c8a5631260102106fbc90f06c5d67e54f08729
suweiteng <suweiteng@51talk.com>
2024-05-09 16:46:57 +0800

    init

---
+++ coOccurrence.py
@@ -0,0 +1,88 @@
+# coding:utf-8
+
+from os import path
+from PIL import Image
+from wordcloud import WordCloud
+from collections import defaultdict, Counter
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import pandas as pd
+stopwords_path = 'userdict/stopword.txt'
+def generate_wordcloud(text):
+
+
+
+
+    # 显示图像
+    # plt.imshow(wc, interpolation='bilinear')
+    # interpolation='bilinear' 表示插值方法为双线性插值
+    # plt.axis("off")# 关掉图像的坐标
+    #plt.show()
+       
+    # 初始化矩阵
+    # print((text))
+    words = text.split()
+    # 统计词频并选择全部词，实际应用中应根据需要筛选高频词汇
+    word_counts = Counter(words)
+    top_words = word_counts.most_common(20)  # 示例中使用所有词，实际应设定如most_common(50)
+
+    # 创建单词到索引的映射，仅包含高频词汇
+    word_to_index = {word: idx for idx, (word, _) in enumerate(top_words)}
+
+    print((word_to_index))
+    #cooc_matrix, vocabulary = build_cooccurrence_matrix(top_words)
+
+    # 由于示例数据较少，可能无法展示出50个高频词，实际应用中应有足够数据
+    # 为了演示，我们直接使用top_words的keys，即高频词列表
+    
+
+    # 过滤words，只保留出现在top_words中的词
+    filtered_words = [word for word in words if word in word_to_index]
+    print((filtered_words))
+    # 构建并显示共现矩阵
+    cooccurrence_matrix = build_cooccurrence_matrix(filtered_words, word_to_index)
+
+    print(cooccurrence_matrix)
+    data1 = pd.DataFrame(cooccurrence_matrix)
+    data1.to_csv('data.csv', index=0, columns=None, encoding='utf_8_sig')
+
+    plt.figure(figsize=(10, 8))
+    plt.rcParams['font.sans-serif']=['Hiragino Sans GB'] # 修改字体
+    plt.rcParams['axes.unicode_minus'] = False # 正常显示负号 
+    sns.heatmap(cooccurrence_matrix, annot=True, fmt='g', cmap='coolwarm', xticklabels=[word for word, _ in top_words], yticklabels=[word for word, _ in top_words])
+    plt.title('Words Co-occurrence Matrix')
+    plt.xlabel('Words')
+    plt.ylabel('Words')
+    plt.show()
+
+# 构建共现矩阵
+def build_cooccurrence_matrix(filtered_words, word_to_index):
+    cooccur_matrix = defaultdict(lambda: defaultdict(int))
+    
+    # 确保只处理存在于高频词汇列表中的词语
+    for i, word1 in enumerate(filtered_words[:-1]):
+        if word1 in word_to_index:
+            word1_idx = word_to_index[word1]
+            for word2 in filtered_words[i+1:]:
+                if word2 in word_to_index:
+                    word2_idx = word_to_index[word2]
+                    cooccur_matrix[word1_idx][word2_idx] += 1
+                    cooccur_matrix[word2_idx][word1_idx] += 1  # 确保共现矩阵对称
+
+    vocab_size = len(word_to_index)
+    matrix = np.zeros((vocab_size, vocab_size), dtype=int)
+    for i, row in enumerate(cooccur_matrix.values()):
+        for j, count in row.items():
+            matrix[i, j] = count
+            
+    return matrix
+
+
+
+
+
+
+# 
--- plotWordcloud.py
+++ plotWordcloud.py
@@ -2,28 +2,32 @@
 
 from os import path
 from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+import numpy as np
 
+stopwords_path = 'userdict/stopword.txt'
 def generate_wordcloud(text):
     '''
     输入文本生成词云,如果是中文文本需要先进行分词处理
     '''
     # 设置显示方式
+    # print(text)
     d=path.dirname(__file__)
     alice_mask = np.array(Image.open(path.join(d, "Images//alice_mask1.jpg")))
     font_path=path.join(d,"font//msyh.ttf")
     wc = WordCloud(
         background_color="white",# 设置背景颜色  "silver"
         color_func=lambda *args, **kwargs: "black" , # 设置字体颜色 
-           max_words=100, # 词云显示的最大词数  
-            width=1200,  # 宽度
-            height=800,   # 高度
+        stopwords=stopwords_path, # 设置停用词 无法为空，因此再设置一次停用词
+            max_words=100, # 词云显示的最大词数  
+            collocations=False, #可防止出现重复
+            # width=600,  # 宽度
+            # height=400,   # 高度
             max_font_size=150, #最大字的最大字体大小
             min_font_size=10, #最小字体大小
            mask=alice_mask,# 设置背景图片       
            font_path=font_path, # 兼容中文字体，不然中文会显示乱码
+           prefer_horizontal=1.01, # 文字横向和竖向之比，当大于1时文字全部横着。默认为0.9
         )
     # 生成词云 
     wc.generate(text)
@@ -31,9 +35,9 @@ def generate_wordcloud(text):
     # 生成的词云图像保存到本地
     wc.to_file(path.join(d, "Images//alice.png"))
 
-    # 显示图像
-    plt.imshow(wc, interpolation='bilinear')
-    # interpolation='bilinear' 表示插值方法为双线性插值
-    plt.axis("off")# 关掉图像的坐标
-    #plt.show()
 
+
+
+
+
+# 
