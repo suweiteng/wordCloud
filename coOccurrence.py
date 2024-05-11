b1c74f52461bd0740c8ca54b41f11bf092b9886b
suweiteng <suweiteng@51talk.com>
2024-05-11 11:13:40 +0800

    1

--- coOccurrence.py
+++ coOccurrence.py
@@ -15,18 +15,17 @@ def generate_wordcloud(text):
     words = text.split()
     # 统计词频并选择全部词，实际应用中应根据需要筛选高频词汇
     word_counts = Counter(words)
-    top_words = word_counts.most_common(40)  # 示例中使用所有词，实际应设定如most_common(50)
+    top_words = word_counts.most_common(31)  # 选取最高频的前N个词语。
 
-    # 创建单词到索引的映射，仅包含高频词汇
-    word_to_index = {word: idx for idx, (word, _) in enumerate(top_words)}
-
+    # 创建单词到索引的映射，仅包含高频词汇，
+    word_to_index = {word: idx for idx, (word, _) in enumerate(top_words)} # word_to_index用于矩阵坐标轴 
     # print((word_to_index))
-  
-    # 过滤words，只保留出现在top_words中的词
-    filtered_words = [word for word in words if word in word_to_index]
-    # print((filtered_words))
+
+    # print(("------------"))
+    # print((words))
+
     # 构建并显示共现矩阵
-    cooccurrence_matrix = build_cooccurrence_matrix(filtered_words, word_to_index)
+    cooccurrence_matrix = build_cooccurrence_matrix(words, word_to_index)
 
     plt.figure(figsize=(10, 8))
     plt.rcParams['font.sans-serif']=['Hiragino Sans GB'] # 修改字体
@@ -41,22 +40,18 @@ def generate_wordcloud(text):
 
 # 构建共现矩阵
 def build_cooccurrence_matrix(filtered_words, word_to_index):
-    cooccur_matrix = defaultdict(lambda: defaultdict(int))
     
-    # 确保只处理存在于高频词汇列表中的词语，并避免重复计数
+    cooccur_matrix = np.zeros((len(word_to_index), len(word_to_index)), dtype=int)
+    
     for i in range(len(filtered_words) - 1):
         word1 = filtered_words[i]
         word2 = filtered_words[i + 1]
         if word1 in word_to_index and word2 in word_to_index:
-            cooccur_matrix[word_to_index[word1]][word_to_index[word2]] += 1
-
-    vocab_size = len(word_to_index)
-    matrix = np.zeros((vocab_size, vocab_size), dtype=int)
-    for i, row in enumerate(cooccur_matrix.values()):
-        for j, count in row.items():
-            matrix[i, j] = count
+            index1, index2 = word_to_index[word1], word_to_index[word2]
+            cooccur_matrix[index1][index2] += 1
+            cooccur_matrix[index2][index1] += 1  # 确保矩阵对称性
             
-    return matrix
+    return cooccur_matrix
 
 
 
